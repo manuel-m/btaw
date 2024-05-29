fbe773b961c7c5f1ae00ee7de6af8f7c2eafa406
Manuel M <discoride.dev@gmail.com>
2024-05-29 13:25:13 +0200

    complete interval test

--- pkg/timeutil/interval_test.go
+++ pkg/timeutil/interval_test.go
@@ -4,9 +4,9 @@ import (
 	"testing"
 )
 
-func TestTimetil(t *testing.T) {
+func TestTimeutil(t *testing.T) {
 
-	t.Run("test 1m", func(t *testing.T) {
+	t.Run("interval 1m", func(t *testing.T) {
 		const wanted int64 = 1000000
 		interval := Interval("1m")
 		got, err := interval.ToMs()
@@ -20,7 +20,21 @@ func TestTimetil(t *testing.T) {
 		}
 	})
 
-	t.Run("supported intervals", func(t *testing.T) {
+	t.Run("unknown interval", func(t *testing.T) {
+		const wanted int64 = 1
+		interval := Interval("!!UNKNOWN_INTERVAL!!")
+		got, err := interval.ToMs()
+
+		if err == nil {
+			t.Errorf("%s|error expected\n", t.Name())
+		}
+
+		if got != wanted {
+			t.Errorf("%s|%d != %d \n", t.Name(), got, wanted)
+		}
+	})
+
+	t.Run("all intervals", func(t *testing.T) {
 
 		intervals := []Interval{"1m", "3m", "5m", "15m", "30m", "1h", "2h", "4h", "8h", "12h", "1d", "3d", "1w"}
 
